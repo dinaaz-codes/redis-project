487dc8320177c38decc56cd4e2c8d4bb1feb4c5e
Dinaaz Shaikh <dinaaz.codes@gmail.com>
2023-02-14 01:18:05 +0530

    add cache service for coin

--- src/controllers/coin.controller.ts
+++ src/controllers/coin.controller.ts
@@ -1,15 +1,20 @@
 import { Request,Response } from "express";
+import { getCache } from "../services/cache.service";
 import { getCoinById } from "../services/coin.service";
 
 export const getCoinDetails = async (req:Request , res : Response) : Promise<any> => {
     try{
+
         const coinId:string = req.params.id;
+        
+        console.time("Execution Time");
+        const coinData = await getCache(coinId,()=>{return getCoinById(coinId)});
+        console.timeEnd("Execution Time");
 
-        const coinData = await getCoinById(coinId);
-
-        return res.status(200).json(coinData);
-    
+        return res.status(200).json(JSON.parse(coinData));
+       
     }catch(error : any){
+        console.log(error);
         res.status(500).json({message:"Something went wrong!"});
     }
 }
---
+++ src/services/cache.service.ts
@@ -0,0 +1,30 @@
+import {createClient} from 'redis';
+
+export const DEFAULT_TIME_TO_LIVE=240;
+
+const redisClient = createClient();
+
+redisClient.connect();
+
+redisClient.on('connect',()=>{
+    console.log('[REDIS] : connected!');
+});
+
+redisClient.on('error',(error)=>{
+    console.log(`[REDIS] : error ${error}`);
+});
+
+export const setCache = async (key:string,toBeCachedValue:string,ttl:number):Promise<string>=> {
+    return await redisClient.setEx(key,ttl,toBeCachedValue);
+}
+
+export const getCache = async (key:string,fetcher:()=>Promise<any>):Promise<string> => {
+    let exists = await redisClient.exists(key);
+
+    if(!exists) {
+            const toBeCached = await fetcher();
+            await setCache(key,JSON.stringify(toBeCached),DEFAULT_TIME_TO_LIVE)
+    }
+
+    return await redisClient.get(key);
+}
